6ef4b2d11ad526706d9a67d9864a05dce4515171
Pierre-Alain Jachiet <pajachiet@gmail.com>
2017-10-24 18:24:57 +0200

    bugfix: use environment variables to connect to postgres from superset

--- volumes/superset/home/bin/init_superset_databases.py
+++ volumes/superset/home/bin/init_superset_databases.py
@@ -14,18 +14,35 @@ logger.addHandler(stream_handler)
 SUPERSET_ADMIN_USERNAME = os.environ.get("SUPERSET_ADMIN_USERNAME")
 SUPERSET_ADMIN_PASSWD = os.environ.get('SUPERSET_ADMIN_PASSWD')
 
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+POSTGRES_PORT = int(os.environ.get('POSTGRES_PORT'))
+POSTGRES_ADMIN_USER = os.environ.get('POSTGRES_ADMIN_USER')
+POSTGRES_ADMIN_PASSWORD = os.environ.get('POSTGRES_ADMIN_PASSWORD')
+
+
 def create_all_databases():
-    cur = psycopg2.connect("postgresql://postgres@postgres:5432").cursor()
+    admin_url = "postgresql://{user}:{password}@{host}:{port}".format(
+        user=POSTGRES_ADMIN_USER,
+        password=POSTGRES_ADMIN_PASSWORD,
+        host=POSTGRES_HOST,
+        port=POSTGRES_PORT
+    )
+    cur = psycopg2.connect(admin_url).cursor()
     cur.execute("SELECT datname FROM pg_database WHERE datistemplate = false;")
     db_list = [t[0] for t in cur if t[0] != 'postgres']
     for db in db_list:
-        logger.info("Create database '{}' in superset".format(db))
+        logger.info("== Create database '{}' in superset".format(db))
         create_superset_database(db)
+
 
 def create_superset_database(db_name):
     data = {
         "database_name": db_name,
-        "sqlalchemy_uri": 'postgres://synchro@postgres/{}'.format(db_name),
+        "sqlalchemy_uri": 'postgres://synchro@{host}:{port}/{db}'.format(
+            host=POSTGRES_HOST,
+            port=POSTGRES_PORT,
+            db=db_name
+        ),
         "expose_in_sqllab": 'y',
         # "cache_timeout": ,
         "extra": """{
