cfc690c28db03823a07dc29b32d28d570925617d
Pierre-Alain Jachiet <pajachiet@gmail.com>
2017-10-25 12:28:16 +0200

    refacto: update demo dumps to download and explain why

--- volumes/mongosource/bin/download_demo_data_dump.sh
+++ volumes/mongosource/bin/download_demo_data_dump.sh
@@ -4,20 +4,24 @@ set -euo pipefail
 mkdir -p /home/data/dump/demo
 cd /home/data/dump/demo
 
-#TODO: Add back catalog.books.json when bug in pymongo-schema will be identified (no mapping)
-#TODO : Modify pymongo-schema to quote / change special keywords
-#TODO: Add back profiles.json when bug in sql syntax will be resolved
-# psycopg2.ProgrammingError: syntax error at or near "user"
-# LINE 1: ...student_id INT ,responseLength INT ,ts TIMESTAMP ,user TEXT ...
-#TODO: Add back products.json when bug in sql syntax will be resolved
-#LINE 1: ...es SERIAL CONSTRAINT PRODUCTS__FOR_PK PRIMARY KEY,for TEXT ,...
-#TODO: Add back restaurant.json with new pymongo-schema correction on ' '
 
+# The following data dump are not downloaded from ozlerhakan/mongodb-json-files because they create bug
+# - restaurant.json contains a key with a space ' ', which create bugs (pymongo-schema and/or mongo-connector-postgresql ?)
+# - catalog.books.json create a bug in pymongo-schema because of the '.' in the name.
+# TODO : identify where the problem is (probably if we split namespace db.collection ?) and correct it
+# - profiles.json create a bug in mongo-connector postgresql because it contains a column named 'user'
+# - products.json create a bug in mongo-connector postgresql because it contains a column named 'for'
+#TODO : Modify mongo-connector-postgresql to quote identifier and distinguish them from keyworkds
+# cf https://www.postgresql.org/docs/current/static/sql-syntax-lexical.html#SQL-SYNTAX-IDENTIFIERS
+# if this work, we could avoid using to_sql_identifier functions in pymongo-schema and mongoconnector.utils
+
+# The following data dump are not downloaded from ozlerhakan/mongodb-json-files because they are not interesting
+# - country.json : the data is in the key names, which makes it difficult to analyze anyway
+# - contries.json : very poor, with only one column
 for file in \
     city_inspections.json \
     companies.json \
     countries.json \
-    country.json \
     covers.json \
     grades.json \
     students.json
@@ -30,12 +34,12 @@ do
     fi
 done
 
-#TODO: Add back with new pymongo-schema correction on '-'
 
-#file=primer-dataset.json
-#if [ ! -e ${file} ]; then
-#    echo "Downloading file '${file}'"
-#    wget https://raw.githubusercontent.com/mongodb/docs-assets/primer-dataset/${file}
-#else
-#    echo "File '${file}' already exists"
-#fi
+
+file=primer-dataset.json
+if [ ! -e ${file} ]; then
+    echo "Downloading file '${file}'"
+    wget https://raw.githubusercontent.com/mongodb/docs-assets/primer-dataset/${file} -O restaurant_primer_dataset.json
+else
+    echo "File '${file}' already exists"
+fi
